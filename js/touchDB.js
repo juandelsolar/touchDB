702170cab94061d99b036b47e01806c2b6334d77
juandelsolar@gmail.com <Juan Francisco>
2014-10-29 18:47:59 -0300

    load config and pouchDB synchronously
    :D

--- js/touchDB.js
+++ js/touchDB.js
@@ -1,20 +1,31 @@
 var touchDB = function (doc_schema) {
 	this.doc_schema = doc_schema;
 	this.db = null;
-	this.loadScript('../js/config.js', this.loadConfig);
-	this.loadScript('../js/libs/pouchdb-3.0.6.min.js', this.createPouchDB);
+	this.require('../js/config.js');
+	this.require('../js/libs/pouchdb-3.0.6.min.js');
+	this.createPouchDB();
 };
 touchDB.prototype.getSchema = function () {
 	return this.doc_schema;	
 };
-touchDB.prototype.loadScript = function (url, callback)
-{
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = url;
-    script.onload = callback;
-    head.appendChild(script);
-};
+touchDB.prototype.require = function (url) {
+    var ajax = new XMLHttpRequest();
+    ajax.open( 'GET', url, false );
+    ajax.onreadystatechange = function () {
+        var script = ajax.response || ajax.responseText;
+        if (ajax.readyState === 4) {
+            switch( ajax.status) {
+                case 200:
+                    eval.apply( window, [script] );
+                    console.log("script loaded: ", url);
+                    break;
+                default:
+                    console.log("ERROR: script not loaded: ", url);
+            }
+        }
+    };
+    ajax.send(null);
+}
 touchDB.prototype.loadConfig = function () {
 	console.log('config loaded!');
 };
@@ -24,7 +35,6 @@ touchDB.prototype.remoteCouchDB = function () {
 touchDB.prototype.createPouchDB = function () {
 	this.db = new PouchDB(schema_database);
 	if(couchDB_servers.length>0) {
-		//this.remoteCouchDB();
-		console.log(this);
+		this.remoteCouchDB();
 	}
 };
